675a09423a6aae4eccbbf1e52aee07d789eb831a
nicu <nicu@bumblebee.(none)>
2012-11-09 21:11:58 +0200

    RED: raise exception to invalid arguments

--- tictactoeplayer_test.rb
+++ tictactoeplayer_test.rb
@@ -25,6 +25,20 @@ class TestPlayer < Test::Unit::TestCase
     end
   end
 
+  def test_does_not_raise_exception_to_hash_argument
+    assert_nothing_raised do
+      tttp = TicTacToePlayer.new
+      tttp.turn({:owned_by_x =>[2,1], :owned_by_y =>[3,5]})
+    end
+  end
+
+  def test_does_raise_exception_to_invalid_arguments
+    assert_raise(ArgumentError) do
+      tttp = TicTacToePlayer.new
+      tttp.turn('test')
+      tttp.turn([2,1,3])
+    end
+  end
 
 end
 
